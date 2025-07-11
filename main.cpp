a5beb13051cf2884ab093b99a1ab5b7c5e9a5684
coconutDreams-56 <cabreraheily@gmail.com>
2025-07-10 23:33:29 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,84 @@
-1
+Project-A---Problem-8.4---Heily-Cabrera
+Write a program that reads in sec01/babynames.txt and produces two files, boynames.txt and girlnames.txt, separating the data for the boys and girls.
+// Programmer: Heily Cabrera 
+// Tester: Audrey Tapia 
+// Group Project A // 
+/***** Members *****/ 
+/ Ashley Jacobson / 
+/ Giannfranco Brance / 
+/ Audrey Tapia / 
+/ Heily Cabrera Guerrero/
+
+#include <iostream>
+#include <stream>
+
+using namespace std; 
+
+int main()
+{
+  string iName =  "sec01/babynames.txt";
+  string oBoyName = "boynames.txt";
+  string oGirlName = "girlnames.txt";
+  string line;
+  int lineNum = 0;
+
+ifstream inputFile(iName);
+if (!inputFile.is_open()) 
+{
+        cerr << "Couldn't open iName file" << endl;
+        return 1;
+}
+
+ofstream boyFile(oBoyName);
+    if (!boyFile.is_open()) 
+    {
+        cerr << "Couldn't open oBoyName file" << endl;
+        return 1;
+    }
+
+ofstream girlFile(oGirlName);
+    if (!girlFile.is_open()) 
+    {
+        cerr << "Couldn't open oGirlName file" << endl;
+        return 1;
+    }
+
+while (getline(inputFile, line)) 
+{
+        lineNumber++; 
+
+        if (line.empty()) 
+        {
+            continue;
+        }
+
+        istringstream iss(line);
+        int rank;           
+        string name;   
+        char genderChar;    
+        int count;          
+
+        if (!(iss >> rank >> name >> genderChar >> count)) {
+            std::cerr << "Couldn't parse line " << lineNumber
+                      << line << endl;
+            continue;
+        }
+
+        if (genderChar == 'M') {
+            boyFile << line << endl; 
+        } else if (genderChar == 'F') {
+            girlFile << line << std::endl; 
+        } else {
+            cerr << "Unknown gender '" << genderChar << lineNumber
+                      << line << endl;
+        }
+    }
+
+    
+    inputFile.close();
+    boyFile.close();
+    girlFile.close();
+
+
+    return 0; 
+}
